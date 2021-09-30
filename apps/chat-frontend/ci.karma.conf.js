f97c10e152e21b2a53114afd0df0879f3e7e111f
Dylan Steele <dylansteele57@gmail.com>
2021-09-30 19:47:46 -0400

    updated ci test commands/configs

--- apps/chat-frontend/ci.karma.conf.js
+++ apps/chat-frontend/ci.karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -12,9 +14,6 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/session-chat'),
       reports: ['html', 'lcovonly', 'text-summary'],
@@ -22,11 +21,15 @@ module.exports = function (config) {
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
-    colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['ChromeHeadless'],
-    singleRun: false,
-    restartOnFileChange: true
+    autoWatch: false,
+    singleRun: true,
+    browsers: ['ChromeHeadless_without_security'],
+    customLaunchers: {
+      ChromeHeadless_without_security: {
+        base: 'ChromeHeadless',
+        flags: ['--disable-web-security', '--disable-site-isolation-trials']
+      }
+    }
   });
 };
