99bd62762b7487d7c0d19ce4e4b228578b6ea83d
Dylan Steele <dylansteele57@gmail.com>
2023-12-22 20:04:49 -0500

    upgraded ang to 16

--- apps/chat-frontend/src/app/services/auth-guard/auth-guard.service.ts
+++ apps/chat-frontend/src/app/services/auth-guard/auth-guard.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { AuthVerificationService } from '../auth-verification/auth-verification.service';
-import { Router, CanActivate } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService implements CanActivate {
-  constructor(public auth: AuthVerificationService, public router: Router) {}
+export class AuthGuardService {
+  constructor(
+    public auth: AuthVerificationService,
+    public router: Router
+  ) {}
 
   canActivate(): boolean {
     if (!this.auth.isAuthenticated()) {
