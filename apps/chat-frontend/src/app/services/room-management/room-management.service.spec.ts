b01084cdc8bb9f790f6fb79604fb2b1a18f66f47
Dylan Steele <dylansteele57@gmail.com>
2024-01-13 23:04:19 -0500

    fixed failing test

--- apps/chat-frontend/src/app/services/room-management/room-management.service.spec.ts
+++ apps/chat-frontend/src/app/services/room-management/room-management.service.spec.ts
@@ -1,12 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RoomManagementService } from './room-management.service';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 
 describe('RoomManagementService', () => {
   let service: RoomManagementService;
 
+  const config: SocketIoConfig = {
+    // url: 'https://ws.dsteele.dev/chat',
+    url: 'localhost:3001/chat',
+    options: {
+      transports: ['websocket'],
+      timeout: 15000
+    }
+  };
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [SocketIoModule.forRoot(config)]
+    });
     service = TestBed.inject(RoomManagementService);
   });
 
