5dde372f49ffa6c5fe40d536ec24b9c0158db7cf
Dylan Steele <dylansteele57@gmail.com>
2024-11-26 21:02:17 -0500

    updated notification tests

--- apps/chat-frontend/src/app/services/notification/notification-service.service.spec.ts
+++ apps/chat-frontend/src/app/services/notification/notification-service.service.spec.ts
@@ -1,19 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 import { Socket } from 'ngx-socket-io';
 
+import { NbToastrService } from '@nebular/theme';
+import { of } from 'rxjs';
+import { NotificationFormat, NotificationTypes } from 'shared-sdk';
 import { NotificationServiceService } from './notification-service.service';
 
 describe('NotificationServiceService', () => {
   let service: NotificationServiceService;
-  let socketIO: jasmine.SpyObj<Socket>;
+  let socketIOMock: jasmine.SpyObj<Socket>;
+  let NbToastrMock: jasmine.SpyObj<NbToastrService>;
 
   beforeEach(() => {
-    socketIO = jasmine.createSpyObj('Socket', ['emit', 'fromEvent']);
+    socketIOMock = jasmine.createSpyObj('Socket', ['emit', 'fromEvent']);
+    NbToastrMock = jasmine.createSpyObj('NbToastrService', ['info', 'danger', 'warning']);
     TestBed.configureTestingModule({
       providers: [
         {
           provide: Socket,
-          useValue: socketIO
+          useValue: socketIOMock
+        },
+        {
+          provide: NbToastrService,
+          useValue: NbToastrMock
         }
       ]
     });
@@ -22,5 +31,35 @@ describe('NotificationServiceService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call showNotification when a socket event is emitted', () => {
+    const spy = spyOn(service, 'showNotification');
+    socketIOMock.fromEvent.and.returnValue(
+      of({
+        type: NotificationTypes.NEW_USER,
+        room: 'room',
+        timestamp: ''
+      } as NotificationFormat)
+    );
+    service.subscribeToNotifications().subscribe();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should show an info based notification if USER_LEFT type is displayed', () => {
+    service.showNotification(NotificationTypes.USER_LEFT);
+    expect(NbToastrMock.info).toHaveBeenCalled();
+  });
+  it('should show an info based notification if NEW_USER type is displayed', () => {
+    service.showNotification(NotificationTypes.NEW_USER);
+    expect(NbToastrMock.info).toHaveBeenCalled();
+  });
+  it('should show an danger based notification if LOGIN_ISSUES type is displayed', () => {
+    service.showNotification(NotificationTypes.LOGIN_ISSUES);
+    expect(NbToastrMock.danger).toHaveBeenCalled();
+  });
+  it('should show an warn based notification if LOGIN_TIMEOUT type is displayed', () => {
+    service.showNotification(NotificationTypes.LOGIN_TIMEOUT);
+    expect(NbToastrMock.warning).toHaveBeenCalled();
   });
 });
--- apps/chat-frontend/src/app/services/notification/notification-service.service.ts
+++ apps/chat-frontend/src/app/services/notification/notification-service.service.ts
@@ -30,6 +30,8 @@ export class NotificationServiceService {
         return this.showInformationalNotification(message, title);
       case 'danger':
         return this.showErrorNotification(message, title);
+      case 'warn':
+        return this.showWarningotification(message, title);
     }
   }
 
@@ -41,6 +43,12 @@ export class NotificationServiceService {
 
   private showErrorNotification(message: string, title?: string) {
     this.toastrService.danger(message, title, {
+      position: NbGlobalPhysicalPosition.TOP_RIGHT
+    });
+  }
+
+  private showWarningotification(message: string, title?: string) {
+    this.toastrService.warning(message, title, {
       position: NbGlobalPhysicalPosition.TOP_RIGHT
     });
   }
